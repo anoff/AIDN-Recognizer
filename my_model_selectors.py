7a0cd6317ca360e65c0a96039ed422c64b51104e
anoff <offenhaeuser@gmail.com>
2018-01-20 12:24:22 +0100

    :star2: feat: cross validation & lint fixes
    :star2: feat:

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -65,7 +65,8 @@ class SelectorBIC(ModelSelector):
     """ select the model with the lowest Bayesian Information Criterion(BIC) score
 
     http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
-    Bayesian information criteria: BIC = -2 * logL + p * logN
+    Bayesian information criteria: BIC = -2 * log_l + p * logN
+        Low = good
     """
 
     def select(self):
@@ -79,14 +80,14 @@ class SelectorBIC(ModelSelector):
         """
         # implement model selection based on BIC scores
         warnings.filterwarnings("ignore", category=DeprecationWarning)
-        N = self.X.shape[0]
+        n = self.X.shape[0]
         best_bic = float('Inf')
-        best_model = NULL
+        best_model = None
         for p in range(self.min_n_components, self.max_n_components):
-            model = self.base_model(num_states=n)
-            logL = model.score(self.X, self.lengths)
-            bic = −2 * logL + p * np.log(N)
-            if bic > best_bic:
+            model = self.base_model(num_states=p)
+            log_l = model.score(self.X, self.lengths)
+            bic = -2 * log_l + p * np.log(n)
+            if bic < best_bic:
                 best_bic = bic
                 best_model = model
         return best_model
@@ -100,13 +101,31 @@ class SelectorDIC(ModelSelector):
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
     https://pdfs.semanticscholar.org/ed3d/7c4a5f607201f3848d4c02dd9ba17c791fc2.pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+        M: number of words
+        log(P(X(i)): log score of self.this_word -> log_l
+        log(P(X(all but i): log score of all words in self.word except .this_word -> log_l_all (all words)
     '''
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        # implement model selection based on DIC scores
+        m = len(self.words)
+        best_dic = float('-Inf') # Higher = Better
+        best_model = None
+        for p in range(self.min_n_components, self.max_n_components):
+            model = self.base_model(num_states=p)
+            log_l = model.score(self.X, self.lengths)
+            # calculate all scores
+            log_l_all = 0
+            for word in self.words:
+                word_x, word_length = self.hwords[word]
+                log_l_all += model.score(word_x, word_length)
+            dic = log_l - 1 / (m - 1) * (log_l_all - log_l)
+            if dic > best_dic:
+                best_dic = dic
+                best_model = model
+        return best_model
 
 
 class SelectorCV(ModelSelector):
@@ -118,4 +137,17 @@ class SelectorCV(ModelSelector):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
         # TODO implement model selection using CV
-        raise NotImplementedError
+        best_cv = float('-Inf')
+        best_model = None
+        for p in range(self.min_n_components, self.max_n_components):
+            split_method = KFold(3)
+            for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                train_x, train_length = combine_sequences(cv_train_idx, self.sequences)
+                test_x, test_length = combine_sequences(cv_test_idx, self.sequences)
+                model = self.base_model(num_states=p).fit(train_x, train_length)
+                cv = model.score(test_x, test_length)
+
+                if cv > best_cv:
+                    best_cv = cv
+                    best_model = model
+        return best_model
